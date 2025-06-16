4c565bf05f440b082fce3b8b692a68473b193209
The Rusty <therustymate@gmail.com>
2025-06-17 00:13:12 +1200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -149,6 +149,13 @@ const CONFIG = {
       to: '2025-04/28',
       companyLink: './unofficial-projects/unofficial_school_latesignin',
     },
+    {
+      company: 'A Public School',
+      position: 'Unofficial Incident Responder',
+      from: '2024-10/08',
+      to: '2025-03/06',
+      companyLink: './unofficial-projects/unofficial_incident_respond',
+    },
   ],
   certifications: [
     // {
