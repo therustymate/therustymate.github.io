504f6264ac8e356c502be8056f5dcc3c542502b8
The Rusty <therustymate@gmail.com>
2025-07-24 10:10:53 +1200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: [], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['therustymate/therustymate', 'therustymate/unofficial-projects'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
