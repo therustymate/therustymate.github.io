ae859fbbed26c9098ef77015d99436c3d8188129
Hyunsung Ra <216290241+therustymate@users.noreply.github.com>
2025-08-25 19:40:31 +1200

    theme changed

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -265,7 +265,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'black',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
