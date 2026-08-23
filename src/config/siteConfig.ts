dfc89878b60d19ce4edfb72a4909aa3fe560bbb8
The Rusty <therustymate@gmail.com>
2026-08-23 22:57:02 +1200

    favicon

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -86,7 +86,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/firefly-32.png",
+			src: "/favicon/favicon.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
